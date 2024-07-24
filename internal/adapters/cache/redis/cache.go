74e968c8a3ad2bb57c2e48b56f2ad8e8a7d16dc6
izruff <rafaelky.dev@gmail.com>
2024-07-24 20:33:18 +0800

    Modify cache interface and require JSON processing

--- internal/adapters/cache/redis/cache.go
+++ internal/adapters/cache/redis/cache.go
@@ -2,6 +2,7 @@ package redis
 
 import (
 	"context"
+	"encoding/json"
 	"time"
 
 	"github.com/redis/go-redis/v9"
@@ -20,23 +21,38 @@ func NewRedisCache(client *redis.Client) *RedisCache {
 }
 
 func (c *RedisCache) Set(key string, value interface{}) error {
-	return c.client.Set(c.ctx, key, value, 0).Err()
+	return c.SetWithExpiry(key, value, 0)
 }
 
 func (c *RedisCache) SetWithExpiry(key string, value interface{}, expiration time.Duration) error {
-	return c.client.Set(c.ctx, key, value, expiration).Err()
+	bytes, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	return c.client.Set(c.ctx, key, bytes, expiration).Err()
 }
 
-func (c *RedisCache) SetAll(kvmap map[string]interface{}) error {
-	return c.client.MSet(c.ctx, kvmap).Err()
+func (c *RedisCache) SetAll(kvMap map[string]interface{}) error {
+	if len(kvMap) == 0 {
+		return nil
+	}
+	kvMapMarshalled := make(map[string]interface{})
+	for k, v := range kvMap {
+		bytes, err := json.Marshal(v)
+		if err != nil {
+			return err
+		}
+		kvMapMarshalled[k] = bytes
+	}
+	return c.client.MSet(c.ctx, kvMapMarshalled).Err()
 }
 
-func (c *RedisCache) Get(key string) (string, error) {
-	return c.client.Get(c.ctx, key).Result()
-}
-
-func (c *RedisCache) GetAll(keys ...string) ([]interface{}, error) {
-	return c.client.MGet(c.ctx, keys...).Result()
+func (c *RedisCache) Get(key string, dest interface{}) error {
+	str, err := c.client.Get(c.ctx, key).Result()
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal([]byte(str), dest)
 }
 
 func (c *RedisCache) DeleteAll(keys ...string) error {
--- internal/core/ports/cache.go
+++ internal/core/ports/cache.go
@@ -5,7 +5,7 @@ import "time"
 type Cache interface {
 	Set(key string, value interface{}) error
 	SetWithExpiry(key string, value interface{}, expiration time.Duration) error
-	SetAll(kvmap map[string]interface{}) error
-	Get(key string) (string, error)
+	SetAll(kvMap map[string]interface{}) error
+	Get(key string, dest interface{}) error
 	DeleteAll(key ...string) error
 }
